0f144cd6cfe87df8c692a28dff9e89f4ff38b064
MaruTerry <marius-labudda@hotmail.com>
2024-10-27 02:48:58 +0200

    Working with audio in python

--- backend/app.py
+++ backend/app.py
@@ -5,25 +5,26 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 
-# Flask setup for HTTP server
 app = Flask(__name__)
 CORS(app)
 
-# Path to the 'songs' folder and allowed file types
 UPLOAD_FOLDER = './backend/songs'
 ALLOWED_EXTENSIONS = {'gp', 'gp3', 'gp4', 'gp5'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 # Endpoint to list song files
 @app.route('/list-songs', methods=['GET'])
 def list_songs():
     songs = []
-    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
+    for filename in os.listdir(UPLOAD_FOLDER):
         if allowed_file(filename):
             songs.append({'name': filename, 'path': f"../backend/songs/{filename}"})
     return jsonify(songs), 200
+
 
 # Endpoint to upload files
 @app.route('/upload', methods=['POST'])
@@ -37,22 +38,23 @@ def upload_file():
     file.save(filepath)
     return jsonify({'message': f'File {file.filename} uploaded successfully!'}), 200
 
+
 # Endpoint to delete files
 @app.route('/delete', methods=['POST'])
 def delete_file():
-    data = request.get_json()  # Get JSON data from request body
+    data = request.get_json()
     if not data or 'filename' not in data:
         return jsonify({'error': 'No file provided'}), 400
     filename = data['filename']
     filepath = os.path.join(UPLOAD_FOLDER, filename)
-    # Check if file exists
     if not os.path.exists(filepath):
         return jsonify({'error': 'File not found'}), 404
     try:
-        os.remove(filepath)  # Deletes the file
+        os.remove(filepath)
         return jsonify({'message': f'File {filename} deleted successfully!'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
 
 # WebSocket setup
 async def websocket_send_data(websocket, path):
@@ -71,11 +73,11 @@ async def start_websocket_server():
         await asyncio.Future()
 
 
-
 async def main():
     loop = asyncio.get_event_loop()
     flask_future = loop.run_in_executor(None, app.run, "localhost", 5000)
     await start_websocket_server()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
